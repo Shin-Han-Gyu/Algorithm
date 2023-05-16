b99ebc44bb0dd2329ecc82ee8479972962ecaed2
신한규 <hanq@kakao.com>
2023-05-16 19:23:13 +0900

    BOJ 10026, 16925 update

--- BOJ/Boj_10026.java
+++ BOJ/Boj_10026.java
@@ -1,1 +1,94 @@
- 
+ import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Boj 10026 gold5
+public class Main {
+    static int N, M, K, ans;
+    static boolean[][] visit;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static char[][] map;
+    static int pi(String a) {
+        return Integer.parseInt(a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = pi(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        map = new char[N][N];
+        for(int i=0; i<N; ++i) {
+            String temp = br.readLine();
+            for(int j=0; j<N; ++j) {
+                map[i][j] = temp.charAt(j);
+            }
+        }
+        visit = new boolean[N][N];
+        for(int i=0; i<N; ++i) {
+            for(int j=0; j<N; ++j) {
+                if(!visit[i][j]) {
+                    solve(i, j, map[i][j]);
+                }
+            }
+        }
+        sb.append(ans + " ");
+        ans = 0;
+        visit = new boolean[N][N];
+        for(int i=0; i<N; ++i) {
+            for(int j=0; j<N; ++j) {
+                if(!visit[i][j]) {
+                    solve1(i, j, map[i][j]);
+                }
+            }
+        }
+        sb.append(ans);
+        System.out.println(sb);
+    }
+    static void solve(int x, int y, char chk) {
+        ++ans;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x,y});
+        visit[x][y] = true;
+        while(!q.isEmpty()) {
+            int[] temp = q.poll();
+
+            for(int i=0; i<4; ++i) {
+                int temp_x = temp[0] + dx[i];
+                int temp_y = temp[1] + dy[i];
+                if(!oob(temp_x, temp_y) && !visit[temp_x][temp_y] && map[temp_x][temp_y]==chk) {
+                    q.add(new int[]{temp_x, temp_y});
+                    visit[temp_x][temp_y] = true;
+                }
+            }
+        }
+    }
+    static void solve1(int x, int y, char chk) {
+        ++ans;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x,y});
+        visit[x][y] = true;
+        while(!q.isEmpty()) {
+            int[] temp = q.poll();
+
+            for(int i=0; i<4; ++i) {
+                int temp_x = temp[0] + dx[i];
+                int temp_y = temp[1] + dy[i];
+                if(!oob(temp_x, temp_y) && !visit[temp_x][temp_y]) {
+                    if(((chk == 'R' || chk == 'G') && (map[temp_x][temp_y] == 'R' || map[temp_x][temp_y] == 'G')) ||
+                            map[temp_x][temp_y]==chk) {
+                        q.add(new int[]{temp_x, temp_y});
+                        visit[temp_x][temp_y] = true;
+                    }
+
+                }
+            }
+        }
+    }
+
+    static boolean oob(int x, int y) {
+        return (x<0 || y<0 || x>N-1 || y>N-1);
+    }
+
+}
--- BOJ/Boj_16925.java
+++ BOJ/Boj_16925.java
@@ -1,1 +1,95 @@
- 
+import java.io.*;
+import java.util.*;
+
+//Boj 16925 gold5
+public class Solution {
+    static int N, M, K, ans;
+    static int pi(String a) {
+        return Integer.parseInt(a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = pi(br.readLine());
+        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<String> head = new ArrayList<>();
+        ArrayList<String> tail = new ArrayList<>();
+        ArrayList<String> head1 = new ArrayList<>();
+        ArrayList<String> tail1 = new ArrayList<>();
+        String[] chk = new String[2];
+        int idx = -1;
+        String sen ="", sen1="";
+        for(int i=0; i<N*2-2; ++i) {
+            String temp = br.readLine();
+            arr.add(temp);
+
+            if(temp.length()==N-1) {
+                chk[++idx] = temp;
+            }
+
+        }
+        String a = chk[0];
+        String b = chk[1];
+        if(N==2) {
+            System.out.println(a+b);
+            System.out.print("PS");
+            return;
+        }
+        sen = b.charAt(0) + a;
+
+        sen1 = a.charAt(0) + b;
+
+        for(int i=1; i<sen.length(); ++i) {
+            head.add(sen.substring(0, i));
+            head1.add(sen1.substring(0, i));
+        }
+
+        for(int i=sen.length()-1; i>0; --i) {
+            tail.add(sen.substring(i, sen.length()));
+            tail1.add(sen1.substring(i, sen1.length()));
+        }
+        int s_=0,p_=0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(sen + "\n");
+        Set<String> set = new HashSet<>();
+        for(String s : arr) {
+            if(head.contains(s)) {
+                if(set.contains(s)){
+                    ++s_;
+                    sb.append("S");
+                    continue;
+                }
+                set.add(s);
+                sb.append("P");
+                ++p_;
+            }
+            else if(tail.contains(s)){
+                sb.append("S");
+                ++s_;
+            }
+        }
+        if(s_!=p_ || s_+p_!=2*N-2) {
+            sb = new StringBuilder();
+            sb.append(sen1 + "\n");
+            set = new HashSet<>();
+
+            for(String s : arr) {
+                if(head1.contains(s)) {
+                    if(set.contains(s)){
+                        sb.append("S");
+                        continue;
+                    }
+                    set.add(s);
+                    sb.append("P");
+                }
+                else if(tail1.contains(s)){
+                    sb.append("S");
+                }
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+}
