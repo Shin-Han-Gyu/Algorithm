12aeb49f2c308f47918479ba24982e50e19851f7
신한규 <hanq@kakao.com>
2023-06-13 22:28:47 +0900

    BOJ 1402 add

---
+++ BOJ/Boj_1402.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+//Boj 1402 silver
+public class Solution {
+    static int K, N, M, ans;
+    static int pi(String a) {
+        return Integer.parseInt(a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = pi(br.readLine());
+        for(int i=0; i<N; ++i) {
+            System.out.println("yes");
+        }
+
+    }
+
+
+}
