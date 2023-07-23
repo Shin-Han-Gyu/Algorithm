6d7e8c316035d92968001444ad173b71ea7f9012
신한규 <hanq@kakao.com>
2023-07-23 13:40:06 +0900

    BOJ 2086 add

---
+++ BOJ/Boj_2086.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+//Boj 2086 gold1
+public class Solution {
+    static int K, N, M, ans;
+    static long Ans;
+    static HashMap<Long, Long> d = new HashMap<Long, Long>();
+    static final long mod = 1000000000;
+    static int pi(String a) {
+        return Integer .parseInt(a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] temp = br.readLine().split(" ");
+
+        Ans = (fibo(Long.parseLong(temp[1])+2) - fibo(Long.parseLong(temp[0])+1)+mod)%mod;
+        System.out.println(Ans);
+    }
+
+    static long fibo(long n)
+    {
+        if (n <= 0)
+            return 0;
+        else if (n == 1 || n == 2)
+            return 1;
+        else if (d.containsKey(n))
+        {
+            return d.get(n);
+        }
+        else
+        {
+            long m;
+            if (n % 2 == 1) {
+                m = (n + 1) / 2;
+                long t1 = fibo(m);
+                long t2 = fibo(m - 1);
+                d.put(n, (t1 * t1 + t2 * t2) % mod);
+            }
+            else {
+                m = n / 2;
+                long t1 = fibo(m - 1);
+                long t2 = fibo(m);
+                d.put(n, ((2 * t1 + t2) * t2) % mod);
+            }
+            return d.get(n);
+        }
+    }
+}
