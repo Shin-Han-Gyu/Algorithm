ac12ffb124d9d465ab8730963e37f4fc2a9a95e4
신한규 <hanq@kakao.com>
2023-08-03 19:58:01 +0900

    BOJ 9625 add

---
+++ BOJ/Boj_9625.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+//Boj 9625 silver5
+public class Solution {
+    static int K, N, M, ans;
+    static boolean[] isPrime;
+    static long Ans;
+    static int pi(String a) {
+        return Integer .parseInt(a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        long[][] dp = new long[46][2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for(int i=2; i<46; ++i) {
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+        K = pi(br.readLine());
+        System.out.println(dp[K][0] + " " + dp[K][1]);
+
+    }
+
+}
